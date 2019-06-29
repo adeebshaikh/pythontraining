1a5ad4dcf0805e2c36d50a8e650eb6ece20acd04
adeebshaikh <51991949+adeebshaikh@users.noreply.github.com>
2019-06-29 14:43:16 +0530

    Add files via upload

---
+++ Untitled (1).py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+x=10
+y=20
+sum=x+y
+print(sum)
+
+
+# In[22]:
+
+
+x=2
+res=x*10
+print(res)
+
+
+# res=x**5
+# print(res)
+
+# In[23]:
+
+
+x=2
+res=x ** 5
+print(res)
+
+
+# In[24]:
+
+
+x=20
+res=x/9
+print(res)
+
+
+# In[25]:
+
+
+x=20
+res=x//9
+print(res)
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+id(r)
+
+
+# In[8]:
+
+
+0b('0101')
+
+
+# In[9]:
+
+
+print("1,10")
+
+
+# In[10]:
+
+
+imag(1)
+
+
+# In[ ]:
+
+
+
+
